386292bafcd2a51113509f0e18a37cbc4d44aec3
Shawn-Shan <shansixioing@uchicago.edu>
2021-05-19 10:19:36 -0500

    fix dependency issue with tensorflow

--- setup.py
+++ setup.py
@@ -76,8 +76,8 @@ setup_requires = []
 
 install_requires = [
     'numpy>=1.19.5',
-    'tensorflow>=2.0.0',
-    'keras>=2.3.1',
+    'tensorflow==2.4.1',
+    'keras==2.4.3',
     'mtcnn',
     'pillow>=7.0.0',
     'bleach>=2.1.0'
